da577e22f97cc98682ed1673fe86a7ce3aec56d3
Hassan Tahseen <ht3aa2001@gmail.com>
2022-04-16 00:56:11 +0300

    first commit2

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ export default function Home() {
       <main>
         <div className={homeStyles.merroirBoxMain}>
           <h2>From Idea to project</h2>
-          <h5>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h5>
+          <h5>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h5>
           <button>Message Us</button>
         </div>
         <div className={homeStyles.contacts}>
@@ -124,19 +124,19 @@ export default function Home() {
           <div className={homeStyles.merroirBoxProject + " designPrj"}>
             <h3>Logo</h3>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  
             </p>
           </div>
           <div className={homeStyles.merroirBoxProject + " programmingPrj"}>
             <h3>Logo</h3>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  
             </p>
           </div>
           <div className={homeStyles.merroirBoxProject + " arduinoPrj"}>
             <h3>Logo</h3>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  
             </p>
           </div>
         </div>
@@ -149,7 +149,7 @@ export default function Home() {
               <Image src={"/me.jpg"} alt="instagram" width="100%" height="100%"/>
             </div>
             <div className={homeStyles.merroirBox}>
-              <p><b>Bob says:</b> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make  </p>
+              <p><b>Bob says:</b> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make  </p>
             </div>
             <div className={homeStyles.bgCircle}></div>
           </div>
