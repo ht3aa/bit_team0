b7137e40b0d01c3b5de136c97b780f3024b9397d
Hassan Tahseen <ht3aa2001@gmail.com>
2022-04-17 17:41:57 +0300

    close to end

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import { useRef } from 'react'
-import homeStyles from '../styles/Home.module.css'
+import homeStyles from '../styles/Home.module.css';
+import heroStyles from '../styles/Hero.module.css';
+import messageStyles from '../styles/Message.module.css';
+import projectsStyles from '../styles/Projects.module.css';
+import customersStyles from '../styles/Customers.module.css';
 import statisticNumber from '../globalFunctions/statisticNumber';
 import { useEffect } from 'react';
 import Image from 'next/dist/client/image';
@@ -18,11 +22,11 @@ export default function Home() {
 
   return (
     <div className={homeStyles.container}>
-      <header className={homeStyles.hero}>
-        <div className={homeStyles.mainContent}>
+      <header className={heroStyles.hero}>
+        <div className={heroStyles.mainContent}>
           <h1>From idea to Project</h1>
           <h5>In Bit we have all you need to make your idea comes true</h5>
-          <form className={homeStyles.mainContentForm}>
+          <form className={heroStyles.mainContentForm}>
             <input type="text" placeholder="Your name"/>
             <input type="text" placeholder="Your email"/>
             <input type="text" placeholder="Your password"/>
@@ -30,194 +34,146 @@ export default function Home() {
           </form>
         </div>
         <i className="fa-solid fa-arrow-down"></i>
-        <div ref={statisticNumberElement} className={homeStyles.statistics}>
-          <div className={homeStyles.box}>
-            <div  className={homeStyles.merroirBox}>
+        <div ref={statisticNumberElement} className={heroStyles.statistics}>
+          <div className={heroStyles.box}>
+            <div  className={heroStyles.merroirBox}>
               <h3></h3>
               <p>Customers</p>
             </div>
-            <div className={homeStyles.bgCircle}></div>
+            <div className={heroStyles.bgCircle}></div>
           </div>                    
-          <div className={homeStyles.box}>
-            <div className={homeStyles.merroirBox}>
+          <div className={heroStyles.box}>
+            <div className={heroStyles.merroirBox}>
               <h3></h3>
               <p>Team Members</p>
             </div>
-            <div className={homeStyles.bgCircle}></div>
+            <div className={heroStyles.bgCircle}></div>
           </div>                    
-          <div className={homeStyles.box}>
-            <div className={homeStyles.merroirBox}>
+          <div className={heroStyles.box}>
+            <div className={heroStyles.merroirBox}>
               <h3></h3>
               <p>Projects</p>
             </div>
-            <div className={homeStyles.bgCircle}></div>
+            <div className={heroStyles.bgCircle}></div>
           </div>
-          <div className={homeStyles.box}>
-            <div className={homeStyles.merroirBox}>
+          <div className={heroStyles.box}>
+            <div className={heroStyles.merroirBox}>
               <h3></h3>
               <p>Followers on Instagram</p>
             </div>
-            <div className={homeStyles.bgCircle}></div>
+            <div className={heroStyles.bgCircle}></div>
           </div>                    
-          <div className={homeStyles.box}>
-            <div className={homeStyles.merroirBox}>
+          <div className={heroStyles.box}>
+            <div className={heroStyles.merroirBox}>
               <h3></h3>
               <p>Followers on Facebook</p>
             </div>
-            <div className={homeStyles.bgCircle}></div>
+            <div className={heroStyles.bgCircle}></div>
           </div>                    
-          <div className={homeStyles.box}>
-            <div className={homeStyles.merroirBox}>
+          <div className={heroStyles.box}>
+            <div className={heroStyles.merroirBox}>
               <h3></h3>
               <p>Services</p>
             </div>
-            <div className={homeStyles.bgCircle}></div>
+            <div className={heroStyles.bgCircle}></div>
           </div>                    
         </div>
       </header>
       <main>
-        <div className={homeStyles.merroirBoxMain}>
+        <div className={messageStyles.messageBox}>
           <h2>From Idea to project</h2>
           <h5>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h5>
           <button>Message Us</button>
         </div>
-        <div className={homeStyles.contacts}>
-          <div className={homeStyles.box}>
-            <div className={homeStyles.merroirBox}>
-              <h3></h3>
-              <p>Instagram</p>
+        <div className={messageStyles.contacts}>
+          <a href='https://instagram.com/bit_team0' target="_blank" rel="noreferrer">
+            <div className={messageStyles.box}>
+              <div className={messageStyles.socialMediaBox}>
+                <h3></h3>
+                <p>Instagram</p>
+              </div>
+              <div className={messageStyles.socialMediaImg}>
+                <Image src={"/instagram.png"} alt="instagram" width="100%" height="100%"/>
+              </div>
             </div>
-            <div className={homeStyles.socialMedia}>
-              <Image src={"/instagram.png"} alt="instagram" width="100%" height="100%"/>
+          </a>
+          <a href="https://www.facebook.com/BIT-for-services-106050172059404/" target="_blank" rel="noreferrer">
+            <div className={messageStyles.box}>
+              <div className={messageStyles.socialMediaBox}>
+                <h3></h3>
+                <p>Facebook</p>
+              </div>
+              <div className={messageStyles.socialMediaImg}>
+                <Image src={"/facebook.png"} alt="facebook" width="100%" height="100%"/>
+              </div>
             </div>
-          </div>
-          <div className={homeStyles.box}>
-            <div className={homeStyles.merroirBox}>
-              <h3></h3>
-              <p>Instagram</p>
+          </a>
+          <a href="https://twitter.com/bit_team0?t=XSM8Of7E12p4lHC3o_X0cw&s=09" target="_blank" rel="noreferrer">
+            <div className={messageStyles.box}>
+              <div className={messageStyles.socialMediaBox}>
+                <h3></h3>
+                <p>Twiter</p>
+              </div>
+              <div className={messageStyles.socialMediaImg}>
+                <Image src={"/twitter.png"} alt="twitter" width="100%" height="100%"/>
+              </div>
             </div>
-            <div className={homeStyles.socialMedia}>
-              <Image src={"/instagram.png"} alt="instagram" width="100%" height="100%"/>
-            </div>
-          </div>
-          <div className={homeStyles.box}>
-            <div className={homeStyles.merroirBox}>
-              <h3></h3>
-              <p>Instagram</p>
-            </div>
-            <div className={homeStyles.socialMedia}>
-              <Image src={"/instagram.png"} alt="instagram" width="100%" height="100%"/>
-            </div>
-          </div>
+          </a>          
         </div>
-        <div className={homeStyles.projectsTypes}>
-          <div className={homeStyles.type1}>
-            <div className={homeStyles.merroirBoxProject + " programmingPrj"}></div>
+        <div className={projectsStyles.projectsTypes}>
+          <div className={projectsStyles.type}>
+            <div className={projectsStyles.smallBox + " programmingPrj"}></div>
             <p>Programming projects</p>
           </div>
         </div>
-        <div className={homeStyles.projectsContainer}>
-          <div className={homeStyles.merroirBoxProject + " designPrj"}>
-            <h3>Logo</h3>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  
-            </p>
-          </div>
-          <div className={homeStyles.merroirBoxProject + " programmingPrj"}>
-            <h3>Logo</h3>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  
-            </p>
-          </div>
-          <div className={homeStyles.merroirBoxProject + " arduinoPrj"}>
+        <div className={projectsStyles.projectsContainer}>
+          <div className={projectsStyles.box + " programmingPrj"}>
             <h3>Logo</h3>
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  
             </p>
           </div>
         </div>
-        <button className={homeStyles.showMoreBtn}>Show more</button>
+        <button className={projectsStyles.showMoreBtn}>Show more</button>
       </main>
-      <div className={homeStyles.customersContainer}>
-        <div ref={sliderRef} className={homeStyles.slider}>
-          <div className={homeStyles.box}>
-            <div className={homeStyles.customersImg}>
+      <div className={customersStyles.customersContainer}>
+        <div ref={sliderRef} className={customersStyles.slider}>
+          <div className={customersStyles.box}>
+            <div className={customersStyles.customersImg}>
               <Image src={"/me.jpg"} alt="instagram" width="100%" height="100%"/>
             </div>
-            <div className={homeStyles.merroirBox}>
-              <p><b>Bob says:</b> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make  </p>
-            </div>
-            <div className={homeStyles.bgCircle}></div>
+            <p><b>Bob says:</b> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make  </p>
           </div>
-                    <div className={homeStyles.box}>
-            <div className={homeStyles.customersImg}>
+          <div className={customersStyles.box}>
+            <div className={customersStyles.customersImg}>
               <Image src={"/me.jpg"} alt="instagram" width="100%" height="100%"/>
             </div>
-            <div className={homeStyles.merroirBox}>
-              <p><b>Bob says:</b> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make  </p>
-            </div>
-            <div className={homeStyles.bgCircle}></div>
+            <p><b>Bob says:</b> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make  </p>
           </div>
-                    <div className={homeStyles.box}>
-            <div className={homeStyles.customersImg}>
+          <div className={customersStyles.box}>
+            <div className={customersStyles.customersImg}>
               <Image src={"/me.jpg"} alt="instagram" width="100%" height="100%"/>
             </div>
-            <div className={homeStyles.merroirBox}>
-              <p><b>Bob says:</b> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make  </p>
-            </div>
-            <div className={homeStyles.bgCircle}></div>
+            <p><b>Bob says:</b> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make  </p>
           </div>
-                    <div className={homeStyles.box}>
-            <div className={homeStyles.customersImg}>
+          <div className={customersStyles.box}>
+            <div className={customersStyles.customersImg}>
               <Image src={"/me.jpg"} alt="instagram" width="100%" height="100%"/>
             </div>
-            <div className={homeStyles.merroirBox}>
-              <p><b>Bob says:</b> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make  </p>
-            </div>
-            <div className={homeStyles.bgCircle}></div>
+            <p><b>Bob says:</b> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make  </p>
           </div>
-                    <div className={homeStyles.box}>
-            <div className={homeStyles.customersImg}>
-              <Image src={"/me.jpg"} alt="instagram" width="100%" height="100%"/>
-            </div>
-            <div className={homeStyles.merroirBox}>
-              <p><b>Bob says:</b> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make  </p>
-            </div>
-            <div className={homeStyles.bgCircle}></div>
-          </div>
-
-                    <div className={homeStyles.box}>
-            <div className={homeStyles.customersImg}>
-              <Image src={"/me.jpg"} alt="instagram" width="100%" height="100%"/>
-            </div>
-            <div className={homeStyles.merroirBox}>
-              <p><b>Bob says:</b> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make  </p>
-            </div>
-            <div className={homeStyles.bgCircle}></div>
-          </div>
-
-                              <div className={homeStyles.box}>
-            <div className={homeStyles.customersImg}>
-              <Image src={"/me.jpg"} alt="instagram" width="100%" height="100%"/>
-            </div>
-            <div className={homeStyles.merroirBox}>
-              <p><b>Bob says:</b> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make  </p>
-            </div>
-            <div className={homeStyles.bgCircle}></div>
-          </div>
-
-          
+  
         </div>
         
       </div>
-        <div className={homeStyles.btnGroup}>
-          <button onClick={() => {
-            backward(sliderRef.current);
-          }}><i className="fa-solid fa-arrow-left"></i></button>
-          <button onClick={() => {
-            forward(sliderRef.current);
-          }}><i className="fa-solid fa-arrow-right"></i></button>
-        </div>
+      <div className={customersStyles.btnGroup}>
+        <button onClick={() => {
+          backward(sliderRef.current);
+        }}><i className="fa-solid fa-arrow-left"></i></button>
+        <button onClick={() => {
+          forward(sliderRef.current);
+        }}><i className="fa-solid fa-arrow-right"></i></button>
+      </div>
       <div id="aboutUs" className={aboutUsStyles.container}>
         <Image src="/me.jpg" width="400" height="400" alt="personal Img"/>
         <div className={aboutUsStyles.content}>
