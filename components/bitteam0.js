6e42f1756710561de87e728ea0d399fc8337d809
Hassan Tahseen <ht3aa2001@gmail.com>
2022-04-29 10:35:09 +0300

    adding social proof

--- components/bitteam0.js
+++ components/bitteam0.js
@@ -6,15 +6,14 @@ export default function Bitteam0() {
   return (
     <section id="bitteam0" className={bitteam0Styles.bitteam0Conatiner}>
         <div className={bitteam0Styles.member}>
-          <Link href="/team/hassanTahseen" passHref >
-            <a>
+            <a href="/hassanTahseen.jpg">
               <Image src="/hassanTahseen.jpg" alt="hassan tahseen" width="500" height="500"/>
             </a>
-          </Link>
           <div className={bitteam0Styles.contact}> 
             <a href="https://www.linkedin.com/in/hassan-tahseen-7a4530224/" target="_blank" rel="noreferrer"><i className="fa-brands fa-linkedin"></i></a>
             <a href="https://instagram.com/ht3aa" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram"></i></a>
             <a href="https://github.com/ht3aa" target="_blank" rel="noreferrer"><i className="fa-brands fa-github"></i></a>
+            <Link href="/team/hassanTahseen" passHref><a><button>details</button></a></Link>
           </div>
         </div>    
     </section>
--- pages/team/hassanTahseen.js
+++ pages/team/hassanTahseen.js
@@ -7,21 +7,40 @@ export default function HassanTahseen() {
   const navigate = useRouter();
   const themesSpanRef = useRef();
   const [theme, setTheme] = useState({
-    mainBgColor: "#023047",
-    borderBoxColor: "white",
-    waterMarkTextColor: "rgba(142, 198, 230, 0.05)",
-    BoxsBgColor: "#023047",
-    headerBoxColor: "white",
-    headerBoxH1Color: "#8EC6E6",
-    headerBoxBtnBgColor: "#FB8500",
-    headerBoxBtnColor: "black",
-    headerBoxBtnHoverBgColor: "#9e5502",
-    gobackBoxColor: "white",
-    mainContentColor: "white",
-    mainContentBtnColor: "black",
-    mainContentBtnBgColor: "#FB8500",
-    mainContentBtnHoverColor: "#9e5502",
+    mainBgColor: "white",
+    borderBoxColor: "#733BF2",
+    waterMarkTextColor: "rgba(49, 27, 188, 0.1)",
+    BoxsBgColor: "white",
+    headerBoxColor: "black",
+    headerBoxH1Color: "#733BF2",
+    headerBoxBtnBgColor: "#733BF2",
+    headerBoxBtnColor: "white",
+    headerBoxBtnHoverBgColor: "#4E28A4",
+    gobackBoxColor: "#733BF2",
+    mainContentColor: "black",
+    mainContentBtnColor: "white",
+    mainContentBtnBgColor: "#733BF2",
+    mainContentBtnHoverColor: "#4E28A4",
   })
+
+  const customersOpinion = [`
+    رأي بشار: حبيبي حسوني الحمدلله
+    بخير دام شفت حروف منك مشتاقلك💜
+    المشروع حلو وعجب الكل وقنعتهم بـ سمنرات سابقة الحمدلله وانت من تحجي ؏ مهندس حسن لا تگلي اتقيد بكلام 5 اسطر لأن انت عملاق وهالشي قليل بحقك من ناحية التعامل ف گايلك انت عبرت النجومية شغل حلو ومرتب واي فجوات ماكو وحتلو اكو ف ما قصرت وما گلت لا ما اكدر بالعكس حتى امور خارج المشروع سعيت بيها واتواصلت ويا كل الاطراف اتمنالك التوفيق بحياتك العلمية والمهنية يا حبيبي💜💜
+  `];
+  const friendsOpinion = [`
+    @mohfalih1 في الأنتسغرام يقول: الصراحة أشوفك دتحاول تسوي الأحسن دائما تحاول تطور ابسك التفاصيل و هذا الصح الله يوفقك ان شاء الله
+    @bme.17.050 في الأنستغرام تقول: شخص طموح يريد يحقق هدف بباله, مو بس هالشي و انما يحب يساعد الغير و يحب يشوفهم مبدعين مثله
+    `]
+  const [customersTextLimt, setCustomersTextLimit] = useState(100);
+  const [friendsTextLimt, setFriendsTextLimit] = useState(100);
+  
+  function showMoreOfCustomerOpinion() {
+    setCustomersTextLimit(customersTextLimt + 200);
+  }
+  function showMoreOfFriendsOpinion() {
+    setFriendsTextLimit(friendsTextLimt + 200);
+  }
 
 function selectTheme(target) {
     if(target.nodeName === "SPAN") {
@@ -88,6 +107,8 @@ function selectTheme(target) {
     }
   }
 
+
+
   return (
     <main style={{ backgroundColor: theme.mainBgColor }} className={hassanStyles.main}>
       <style jsx>{`
@@ -138,8 +159,8 @@ function selectTheme(target) {
         </div>
         <header id="headerBox" className={hassanStyles.headerBox}>
           <h1>Hassan</h1>
-          <h4>Strong, Kind, Better</h4>
-          <button>Call Me</button>
+          <h4>Programmer, Kind, Creative</h4>
+          <a href="tel:07708246418"><button>Call Me</button></a>
         </header>
         <div id="themeBox" ref={themesSpanRef} onClick={(e) => selectTheme(e.target)} className={hassanStyles.themeBox}>
           <span id="blue" className="themeActive"></span>
@@ -168,26 +189,22 @@ function selectTheme(target) {
           <div className={hassanStyles.contentLeft}>
             <div>
               <h2>Why Hassan?</h2>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the</p>
-              <button>Call me</button>
+              <p className="ar">{ friendsOpinion[0].substring(0, friendsTextLimt) }</p>
+              {/* <p className="ar">{ friendsOpinion[1].substring(0, friendsTextLimt) }</p> */}
+              <button onClick={showMoreOfFriendsOpinion}>Show more</button>
             </div>
             <div>
-              <h2>Why Hassan?</h2>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the</p>
-              <button>Call me</button>
+              <h2>Trust worthy?</h2>
+              <p className="ar"> { customersOpinion[0].substring(0, customersTextLimt) }</p>
+              <button onClick={showMoreOfCustomerOpinion}>Show more</button>
             </div>
           </div>
 
           <div className={hassanStyles.contentRight}>
             <div>
-              <h2>Why Hassan?</h2>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the</p>
-              <button>Call me</button>
-            </div>
-            <div>
-              <h2>Why Hassan?</h2>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the</p>
-              <button>Call me</button>
+              <h2>his background?</h2>
+              <p>hassan tahseen abd-alretha, 21 years old, lives in Iraq/Baghdad. student at the university of technology biomedical engineering department. from a family of 4 members, a passion for programming and learning it. allhamdallah.</p>
+              <a href="tel:07708246418"><button>Call Me</button></a>
             </div>
           </div>
         </div>
